ea8c23c9a688047980c571237ec3ae472ea38518
Chell0 <machelgabriel@gmail.com>
2024-11-04 11:24:22 +0300

    chore: - Create Integrative component. - Create YouTube component.

---
+++ src/components/Integrative/YouTubeSection/YouTubeSection.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+import YouTube, { YouTubeProps } from 'react-youtube';
+import {YouTubeSectionProps} from "@/app/lib/interface";
+
+
+
+export  default function YouTubeSection({ videoId }:YouTubeSectionProps) {
+    const onPlayerReady: YouTubeProps['onReady'] = (event) => {
+        event.target.pauseVideo();
+    };
+
+    const playerOptions: YouTubeProps['opts'] = {
+        height: '360',
+        width: '640',
+        playerVars: {
+            autoplay: 0,
+        },
+    };
+
+    return (
+        <section className={`rounded-xl py-10`}>
+            {/* YouTube Video Player */}
+            <YouTube videoId={videoId} opts={playerOptions} onReady={onPlayerReady} />
+        </section>
+    );
+};
