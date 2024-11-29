5d9bf90badb4be52b914d67dac451af7e090bef1
Machel G Odhiambo <machelgabriel@gmail.com>
2024-11-29 19:46:01 +0300

    chore: Change colour.

--- src/components/Newsletter/Newsletter.tsx
+++ src/components/Newsletter/Newsletter.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export default function NewsLetter() {
     return (
-        <div className="grid grid-rows-2 items-center justify-center max-w-lg px-6 py-4 p-8 border border-gray-300 bg-title-bg shadow-xl rounded-3xl mt-20">
-            <p className="text-orange-600 text-xl lg:text-2xl md:text-md sm:text-sm font-bold text-center">
+        <div className="grid grid-rows-2 items-center justify-center max-w-lg px-6 py-4 p-8 bg-main-bg shadow-xl rounded-3xl mt-20">
+            <p className="text-[#ffb400] text-xl lg:text-2xl md:text-md sm:text-sm font-bold text-center">
                 Anmeldung zum Newsletter
             </p>
             <div className="flex justify-center mt-5">
@@ -11,9 +11,9 @@ export default function NewsLetter() {
                     href="#"
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="bg-white hover:bg-blended-gradient hover:text-white text-orange-600 font-semibold text-md no-underline py-2 px-4 shadow-xl rounded-2xl"
+                    className="bg-[#ffb400] hover:bg-blended-gradient hover:text-white text-white font-bold text-md no-underline py-2 px-4 shadow-xl rounded-2xl"
                 >
-                    Anmelden
+                    ANMELDEN
                 </Link>
             </div>
         </div>
