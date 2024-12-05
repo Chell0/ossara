af1abf89cda441716d7e212fadedf364f68c9a9e
Machel G Odhiambo <machelgabriel@gmail.com>
2024-12-05 23:13:30 +0300

    chore: Fix Type Error to match the required types of a Next.js Layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,59 +1,79 @@
-import '@fortawesome/fontawesome-free/css/all.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import type { Metadata } from "next";
-import { Acme, Bebas_Neue, Fjalla_One, Galdeano, Marko_One, Open_Sans, Roboto_Serif, Rubik } from "next/font/google";
+import {
+  Acme,
+  Bebas_Neue,
+  Fjalla_One,
+  Galdeano,
+  Marko_One,
+  Open_Sans,
+  Roboto_Serif,
+  Rubik,
+} from "next/font/google";
 import React from "react";
 import "../styles/globals.css";
 
-export const acme = Acme({
-	subsets: ["latin"],
-	weight: "400",
-});
-export  const openSans = Open_Sans({ subsets: ["latin"] });
-
-
-export const fjallaOne = Fjalla_One({
-	subsets: ["latin"],
-	weight: "400",
+const acme = Acme({
+  subsets: ["latin"],
+  weight: "400",
 });
 
-export const bebasNeue = Bebas_Neue({
-	subsets: ["latin"],
-	weight: "400",
+const openSans = Open_Sans({ subsets: ["latin"] });
+
+const fjallaOne = Fjalla_One({
+  subsets: ["latin"],
+  weight: "400",
 });
 
-export const markoOne = Marko_One({
-	subsets: ["latin"],
-	weight: "400",
+const bebasNeue = Bebas_Neue({
+  subsets: ["latin"],
+  weight: "400",
 });
 
-export const galdeano = Galdeano({
-	subsets: ["latin"],
-	weight: "400",
+const markoOne = Marko_One({
+  subsets: ["latin"],
+  weight: "400",
 });
 
-export const robotoSerif = Roboto_Serif({
-	subsets: ["latin"],
-	weight: "400",
+const galdeano = Galdeano({
+  subsets: ["latin"],
+  weight: "400",
 });
 
-export const rubik = Rubik({
-	subsets: ["latin"],
-	weight: "400",
+const robotoSerif = Roboto_Serif({
+  subsets: ["latin"],
+  weight: "400",
 });
 
+const rubik = Rubik({
+  subsets: ["latin"],
+  weight: "400",
+});
 
 export const metadata: Metadata = {
-	title: "Ossara",
-	description: "Ossara",
+  title: "Ossara",
+  description:
+    "Welcome to Ossara!  Highlighting engagement and projects in Hamburg's decolonization process.",
+  keywords: [
+    "machtbewusst",
+    "decolonization",
+    "Hamburg",
+    "civil society",
+    "engagement",
+    "projects",
+    "power-critical perspectives",
+    "decolonial perspectives",
+  ],
 };
 
-export default function RootLayout({ children,
+export default function RootLayout({
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-			<body	className={fjallaOne.className}>{children}</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body className={fjallaOne.className}>{children}</body>
+    </html>
+  );
 }
