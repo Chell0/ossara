fcdf82c069cdd4cb13858de524b107bac8b47753
Machel G Odhiambo <machelgabriel@gmail.com>
2024-12-06 05:44:21 +0300

    chore: Remove the Ansprechperson section.

--- src/components/Bildungsarbeit/Bildungsarbeit.tsx
+++ src/components/Bildungsarbeit/Bildungsarbeit.tsx
@@ -98,53 +98,6 @@ export default function Bildungsarbeit() {
             />
           </div>
         </section>
-        {/* // TODO: DELETE THIS PORTION */}
-        {/* Ansprechppartner */}
-        <section className="w-full flex items-center justify-center mt-28 sm:px-2">
-          <div className="bg-blended-gradient shadow-lg rounded-lg max-w-md sm:px-4">
-            <h2 className="text-white text-xl py-4 px-4 sm:text-2xl md:text3xl lg:text-4xl font-semibold text-center">
-              Ansprechpartner
-            </h2>
-          </div>
-        </section>
-
-        <section className="flex flex-col md:flex-row items-center gap-10 py-10 mx-10 mt-20 mb-20">
-          <div className="w-full md:w-1/2 flex-shrink-0 flex justify-center">
-            <Image
-              src={`/bgs/img1.jpg`}
-              alt="About Photo"
-              width={400}
-              height={400}
-              className="rounded-xl shadow-xl object-cover sm:w-40 sm:h-40 md:w-72 md:h-72 lg:w-84 lg:h-84"
-            />
-          </div>
-          <div className="w-full md:w-1/2 bg-gray-200 p-4 rounded-md max-w-sm mx-auto">
-            <div className={`space-y-2 p-1 sm:p-2 md:p-3 lg:p-6`}>
-              <p
-                className={`text-sm sm:text-md md:text-lg lg:text-xl font-bold`}
-              >
-                Kontakt:{" "}
-                <span className={`font-medium text-orange-600`}>John Doe</span>
-              </p>
-              <p
-                className={`text-sm sm:text-md md:text-lg lg:text-xl font-bold`}
-              >
-                Email:{" "}
-                <span className={`font-medium text-orange-600`}>
-                  johndoe[a]ossara.de
-                </span>
-              </p>
-              <p
-                className={`text-sm sm:text-md md:text-lg lg:text-xl font-bold`}
-              >
-                Cell:{" "}
-                <span className={`font-medium text-orange-600`}>
-                  (+49) 123-4567-8910
-                </span>
-              </p>
-            </div>
-          </div>
-        </section>
 
         <section className="flex flex-col md:flex-row items-center gap-10 py-10 mx-10 mt-20 mb-20">
           <div className="w-full md:w-1/2">
