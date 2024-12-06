bf702516b35b7f34a861a772936dc5b005ae4f72
Machel G Odhiambo <machelgabriel@gmail.com>
2024-12-07 00:35:52 +0300

    chore: Remove unused fonts.

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -1,19 +1,4 @@
-import {
-  Acme,
-  Bebas_Neue,
-  Fjalla_One,
-  Galdeano,
-  Manrope,
-  Marko_One,
-  Open_Sans,
-  Roboto_Serif,
-} from "next/font/google";
-
-export const acme = Acme({
-  subsets: ["latin"],
-  display: "swap",
-  weight: "400",
-});
+import { Bebas_Neue, Manrope } from "next/font/google";
 
 export const bebasNeue = Bebas_Neue({
   subsets: ["latin"],
@@ -21,37 +6,7 @@ export const bebasNeue = Bebas_Neue({
   weight: "400",
 });
 
-export const fjallaOne = Fjalla_One({
-  subsets: ["latin"],
-  display: "swap",
-  weight: "400",
-});
-
-export const galdeano = Galdeano({
-  subsets: ["latin"],
-  display: "swap",
-  weight: "400",
-});
-
 export const manrope = Manrope({
-  subsets: ["latin"],
-  display: "swap",
-  weight: "400",
-});
-
-export const markoOne = Marko_One({
-  subsets: ["latin"],
-  display: "swap",
-  weight: "400",
-});
-
-export const openSans = Open_Sans({
-  subsets: ["latin"],
-  display: "swap",
-  weight: "400",
-});
-
-export const robotoSerif = Roboto_Serif({
   subsets: ["latin"],
   display: "swap",
   weight: "400",
