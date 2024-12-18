17c621f1ec5a1aeaf095738d61bf47a19509b229
Machel G Odhiambo <machelgabriel@gmail.com>
2024-12-19 02:05:04 +0300

    chore: - Replace placeholder text. - Update section design.

--- src/components/Bildungsarbeit/Bildungsarbeit.tsx
+++ src/components/Bildungsarbeit/Bildungsarbeit.tsx
@@ -8,27 +8,27 @@ export default function Bildungsarbeit() {
       <div className="container p-6 mx-auto">
         <header className="text-[#ffb400] py-12 text-center">
           <h1
-            className={`${bebasNeue.className} drop-shadow-xl text-header font-bold leading-tight uppercase`}
+            className={`${bebasNeue.className} drop-shadow-xl text-8xl font-bold leading-tight uppercase`}
           >
             Dekoloniale Bildungsarbeit
           </h1>
         </header>
 
-        <section className="flex flex-col items-center py-10 mx-10 mt-20 mb-20 md:flex-row gap-10">
+        <section className="flex flex-col items-center py-10 mx-10 mt-20 mb-20 md:flex-row gap-20">
           <div className="flex-shrink-0 w-full md:w-1/2">
             <Image
               src={`/bgs/img1.jpg`}
               alt="About Photo"
-              width={560}
-              height={480}
+              width={480}
+              height={520}
               quality={100}
               className="w-full h-auto rounded-lg shadow-lg"
             />
           </div>
           <div className="w-full md:w-1/2">
-            <p className="mt-4 text-sm text-gray-800">
+            <p className="mt-4 text-base text-gray-800">
               Ossara e.V. setzt sich aktiv für die Sichtbarmachung und
-              Auseinandersetzung mit <strong>kolonialen Erblasten</strong> und
+              Auseinandersetzung mit <strong>kolonialen Erblasten</strong> und{" "}
               <strong>Dekolonialisierungsprozessen</strong> ein. Ein zentraler
               Aspekt unserer Arbeit ist es,{" "}
               <strong>dekoloniale Perspektiven</strong> sichtbar zu machen und
@@ -38,15 +38,15 @@ export default function Bildungsarbeit() {
               in der <strong>Vernetzung mit anderen Initiativen</strong>, um
               einen gemeinsamen Austausch zu fördern und eine breite Bewegung
               für soziale Gerechtigkeit zu unterstützen.
-              <br />
-              <br />
+            </p>
+            <p className="mt-2 text-base text-gray-800">
               Wir bieten verschiedene Formate an, die auf eine kritische
               Auseinandersetzung mit kolonialen Strukturen abzielen. Dazu
               gehören unter anderem Veranstaltungen und{" "}
               <strong>Podiumsdiskussionen</strong>, die sich mit den
               historischen und aktuellen Dimensionen des Kolonialismus befassen.
-              <br />
-              <br />
+            </p>
+            <p className="mt-2 text-base text-gray-800">
               Durch unsere Bildungsarbeit möchten wir dazu beitragen, den
               <strong>kolonialen Blick</strong> zu hinterfragen und zu einer
               gerechteren, inklusiveren Gesellschaft beizutragen.
@@ -54,26 +54,30 @@ export default function Bildungsarbeit() {
           </div>
         </section>
 
-        {/* Machtbewusst */}
-        <section className="flex flex-col items-center py-10 mx-10 mt-20 mb-20 md:flex-row gap-10">
-          <div className="w-full md:w-1/2">
+        {/* MachtBewusst */}
+        <section className="flex items-center justify-center w-full py-10 mt-10 ">
+          <div className="max-w-3xl mx-auto">
             <h2
-              className={`${bebasNeue.className} drop-shadow-lg text-5xl font-bold text-[#ffb400] uppercase`}
+              className={`${bebasNeue.className} drop-shadow-lg text-6xl font-bold text-[#ffb400] uppercase`}
             >
               <Link
                 href="https://machtbewusst-hamburg.de"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                Machtbewusst-Hamburg
+                MachtBewusst Hamburg
               </Link>
             </h2>
-            <p className="mt-4 text-sm text-gray-800">
+          </div>
+        </section>
+        <section className="flex flex-col items-center py-10 mx-10 mb-20 md:flex-row gap-20">
+          <div className="w-full md:w-1/2">
+            <p className="text-base text-gray-800">
               Bist du auf der Suche nach Themen zur Dekolonisierung? Möchtest du
               wissen, was in Hamburg passiert und wer sich aktiv für
               Veränderungen einsetzt? Dann bist du bei uns genau richtig!
-              <br />
-              <br />
+            </p>
+            <p className="mt-2 text-base text-gray-800">
               In unserem Blog „MachtBewusst Hamburg“ dreht sich alles um
               machtkritische und dekoloniale Perspektiven in der Hamburger
               Zivilgesellschaft. Wir beleuchten das Engagement von Initiativen,
@@ -81,8 +85,8 @@ export default function Bildungsarbeit() {
               in Hamburg vorantreiben. Hier findest du spannende Beiträge,
               wertvolles Fachwissen und inspirierende Geschichten, die zeigen,
               wie der Wandel in unserer Stadt gestaltet wird.
-              <br />
-              <br />
+            </p>
+            <p className="mt-2 text-base text-gray-800">
               Komm vorbei, lass dich von unseren Inhalten inspirieren und
               informiere dich über die vielfältigen Projekte und Aktionen, die
               das Ziel haben, gesellschaftliche Ungleichheiten zu bekämpfen und
@@ -90,14 +94,20 @@ export default function Bildungsarbeit() {
             </p>
           </div>
           <div className="flex-shrink-0 w-full md:w-1/2">
-            <Image
-              src={`/bgs/img1.jpg`}
-              alt="About Photo"
-              width={560}
-              height={480}
-              quality={100}
-              className="w-full h-auto rounded-lg shadow-lg"
-            />
+            <Link
+              href="https://machtbewusst-hamburg.de"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                src={`/sections/machtbewusst.png`}
+                alt="About Photo"
+                width={520}
+                height={720}
+                quality={100}
+                className="w-full h-auto rounded-lg shadow-lg"
+              />
+            </Link>
           </div>
         </section>
 
@@ -105,25 +115,25 @@ export default function Bildungsarbeit() {
         <section className="flex items-center justify-center w-full py-10 mt-10 ">
           <div className="max-w-3xl mx-auto">
             <h2
-              className={`${bebasNeue.className} text-[#ffb400] mt-20 text-5xl sm:text-3xl md:text-4xl lg:text-6xl font-bold text-center drop-shadow-md`}
+              className={`${bebasNeue.className} drop-shadow-lg text-6xl font-bold text-[#ffb400] uppercase`}
             >
-              Eine Welt-Promotor*innen Program
+              Eine Welt-Promotor*innen Programm
             </h2>
           </div>
         </section>
-        <section className="flex flex-col items-center py-10 mx-10 mt-10 mb-20 md:flex-row gap-10">
+        <section className="flex flex-col items-center py-10 mx-10 mb-20 md:flex-row gap-10">
           <div className="flex-shrink-0 w-full md:w-1/2">
             <Image
-              src={`/bgs/img1.jpg`}
-              alt="About Photo"
-              width={560}
-              height={480}
+              src={`/sections/eine-welt-fach-promotorinnen.jpg`}
+              alt="Eine Welt-Promotorinnen Photo"
+              width={520}
+              height={720}
               quality={100}
               className="w-full h-auto rounded-lg shadow-lg"
             />
           </div>
           <div className="w-full md:w-1/2">
-            <p className="mt-4 text-sm text-gray-800">
+            <p className="text-base text-gray-800">
               Die Fachstelle dekoloniale Perspektiven im entwicklungspolitischen
               Engagement ist Teil des bundesweiten Eine
               Welt-Promotor:innen-Programms, welches in Hamburg von
@@ -136,13 +146,13 @@ export default function Bildungsarbeit() {
               UN-Nachhaltigkeitsziele bei, insbesondere zur Reduzierung von
               Ungleichheiten (Ziel 10). Der Fokus liegt auf der Vernetzung und
               Beratung von zivilgesellschaftlichen Initiativen und Akteur:innen.
-              <br />
-              <br />
+            </p>
+            <p className="mt-2 text-base text-gray-800">
               Die Fachstelle Dekolonisierung ist Teil des bundesweiten Eine
               Welt-Promotor:innen-Programms, das in Hamburg von hamburg.global
               koordiniert wird.
-              <br />
-              <br />
+            </p>
+            <p className="mt-2 text-base text-gray-800">
               Weitere Informationen finden Sie{" "}
               <Link
                 href={`/eine_welt_promotorinnen`}
