a30697fc8fd9a876c36bfd3e3bf5059e5b04df4d
chell0 <machelgabriel@gmail.com>
2025-01-16 15:18:24 +0300

    chore: Add background colour.

--- src/app/uber/page.tsx
+++ src/app/uber/page.tsx
@@ -1,16 +1,15 @@
-
 import AboutPage from "@/components/AboutPage/AboutPage";
 import Footer from "@/components/Footer/Footer";
 import NavBar from "@/components/NavBar/NavBar";
 
 export default function Page() {
-	return (
-		<div>
-			<main className="p-6">
-				<NavBar />
-				<AboutPage />
-				<Footer />
-			</main>
-		</div>
-	);
+  return (
+    <div>
+      <main className="bg-[#fef5db]">
+        <NavBar />
+        <AboutPage />
+        <Footer />
+      </main>
+    </div>
+  );
 }
