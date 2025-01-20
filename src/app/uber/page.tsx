2c33a0cedb524b0de304342c29603ff9a4366afa
chell0 <machelgabriel@gmail.com>
2025-01-20 13:49:15 +0300

    chore: - Add bg-colour. - Remove main styling.

--- src/app/uber/page.tsx
+++ src/app/uber/page.tsx
@@ -1,13 +1,13 @@
-import AboutPage from "@/components/AboutPage/AboutPage";
 import Footer from "@/components/Footer/Footer";
 import NavBar from "@/components/NavBar/NavBar";
+import UberUns from "@/components/UberUns/UberUns";
 
 export default function Page() {
   return (
     <div>
       <main className="bg-[#fef5db]">
         <NavBar />
-        <AboutPage />
+        <UberUns />
         <Footer />
       </main>
     </div>
