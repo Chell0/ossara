fef6ae14325daaf56de8df663890d170d21d1e04
chell0 <machelgabriel@gmail.com>
2025-01-23 04:15:40 +0300

    Chore: - Create Projects page. - Import Projects component.

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,15 @@
+import Footer from "@/components/Footer/Footer";
+import NavBar from "@/components/NavBar/NavBar";
+import Projects from "@/components/Projects/Projects";
+
+export default function Page() {
+  return (
+    <div>
+      <main className="bg-[#fad6bb">
+        <NavBar />
+        <Projects />
+        <Footer />
+      </main>
+    </div>
+  );
+}
