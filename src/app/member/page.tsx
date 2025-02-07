e171d5506867fcf65deb7930b631739564600457
Machel G O <machelgabriel@gmail.com>
2025-02-07 06:09:09 +0300

    chore: - Create BecomeMember component. - Create MemberForm component. - Import MemberForm into BecomeMember component. - Create member page. - Import BecomeMember component into the member page.

---
+++ src/app/member/page.tsx
@@ -0,0 +1,15 @@
+import BecomeMember from "@/components/BecomeMember/BecomeMember";
+import Footer from "@/components/Footer/Footer";
+import NavBar from "@/components/NavBar/NavBar";
+
+export default function Page() {
+  return (
+    <div>
+      <main className="bg-[#fef5db]">
+        <NavBar />
+        <BecomeMember />
+        <Footer />
+      </main>
+    </div>
+  );
+}
