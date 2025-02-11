31a1a6987ec5eed3e05159699b54a3571a201a0e
Machel G O <machelgabriel@gmail.com>
2025-02-11 03:08:59 +0300

    chore: - Update Page layout. - Change backgroundColor. - Change font-size.

--- src/components/FördernUndSpenden/DonationForm/DonationForm.tsx
+++ src/components/FördernUndSpenden/DonationForm/DonationForm.tsx
@@ -1,20 +1,33 @@
+import Link from "next/link";
+
 export default function DonationForm() {
   return (
     <>
-      <section className="px-4 py-20 mt-10 sm:px-8 lg:px-16">
-        <div id="donation-form" className="max-w-4xl mx-auto text-center">
-          <h3
-            className={`text-[#ffb400] text-5xl sm:text-2xl md:text-4xl lg:text-5xl font-semibold text-center drop-shadow-sm`}
+      <section className="h-screen w-full relative bg-[#fcc41b]">
+        <div
+          id="donation-form"
+          className="absolute inset-0 flex flex-col items-center justify-center pl-10 gap-8"
+        >
+          <h2
+            className={`text-[#fef5db] text-6xl sm:text-4xl md:text-5xl lg:text-7xl font-semibold text-center drop-shadow-sm`}
           >
             Deine Spende
-          </h3>
-          <p className="mt-4 text-gray-800 text-base">
+          </h2>
+          <p className="max-w-xl mt-4 text-gray-700 text-base">
             Mit Deiner Hilfe können wir wichtige Arbeit leisten, die sonst nicht
             möglich wäre. <b>OSSARA e.V.</b> empowert Schwarze Menschen,
             Menschen afrikanischer Herkunft. Dafür brauchen wir einen langen
             Atem! Hier kannst Du uns einmalig mit Deiner Spende oder regelmäßig
             als <b>OSSARA Community Support</b> unterstützen:
           </p>
+          {/* Paypal Link */}
+          <Link
+            href="#paypal"
+            aria-label="Paypal Link"
+            className="px-8 py-4 text-xl font-medium text-[#fcc41b] bg-[#fef5db] rounded-lg hover:bg-[#e6a200] transition-colors"
+          >
+            Spenden
+          </Link>
         </div>
       </section>
     </>
