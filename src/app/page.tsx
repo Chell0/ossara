07c4370e316bbbf577160a2df02bab0d7a880f59
Machel G O <machelgabriel@gmail.com>
2025-02-12 07:22:00 +0300

    fix: Fix eslint errors.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
+import Link from "next/link";
 
 import Footer from "@/components/Footer/Footer";
 import Gallery from "@/components/Home/Gallery/Gallery";
 import HeaderCarousel from "@/components/Home/HeaderCarousel/HeaderCarousel";
 import NavBar from "@/components/NavBar/NavBar";
-import Link from "next/link";
 
 export default function Home() {
   return (
