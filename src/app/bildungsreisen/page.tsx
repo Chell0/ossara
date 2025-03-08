a155d5cb4a773aff1cd2ee22c533ec986842730e
Machel G O <machelgabriel@gmail.com>
2025-03-08 04:25:59 +0300

    Chore: Create Bildungsreisen page.

---
+++ src/app/bildungsreisen/page.tsx
@@ -0,0 +1,15 @@
+import Bildungsreisen from "@/components/Bildungsreisen/Bildungsreisen";
+import Footer from "@/components/Footer/Footer";
+import NavBar from "@/components/NavBar/NavBar";
+
+export default function Page() {
+  return (
+    <div>
+      <main className="bg-[#fff6ea]">
+        <NavBar />
+        <Bildungsreisen />
+        <Footer />
+      </main>
+    </div>
+  );
+}
