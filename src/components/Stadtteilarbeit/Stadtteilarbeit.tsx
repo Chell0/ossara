8f3561e48ace5cf1c6810ec4a169916d747554d2
Machel G O <machelgabriel@gmail.com>
2025-03-08 04:14:22 +0300

    chore: Update Header Section.

--- src/components/Stadtteilarbeit/Stadtteilarbeit.tsx
+++ src/components/Stadtteilarbeit/Stadtteilarbeit.tsx
@@ -7,32 +7,29 @@ export default function Stadtteilarbeit() {
       <div>
         {/* Header Section */}
         <header
-          className="h-screen w-full bg-cover bg-center relative p-4 sm:p-6 md:p-8 lg:p-10"
+          className="h-screen w-full bg-cover bg-center relative p-4"
           style={{ backgroundImage: "url(/integrative/img-05.jpg)" }}
         >
           <div className="absolute top-0 left-0 right-0 inset-0 flex items-start justify-start">
-            <h1 className="text-[#fcc41b] drop-shadow-2xl text-5xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl font-extrabold leading-tight uppercase p-2 sm:p-4 md:p-6 lg:p-8 mt-2 sm:mt-4 md:mt-6 lg:mt-8">
-              Stadtteil. <br />
+            <h1 className="text-[#fcc41b] text-5xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl font-extrabold leading-tight uppercase p-2 sm:p-4 md:p-6 lg:p-8 mt-2 sm:mt-4 md:mt-6 lg:mt-8">
+              Stadtteil.
+              <br />
               arbeit.
             </h1>
           </div>
         </header>
 
         <section className="relative w-full min-h-screen text-[#fcc41b]">
-          <div className="flex flex-col pt-6 md:pt-8 lg:pt-12">
-            <div className="w-full mx-auto px-4 sm:px-6 lg:px-8">
-              <p
-                className="text-3xl xs:text-4xl sm:text-5xl md:text-6xl lg:text-7xl 
-                font-semibold leading-normal
-                tracking-tight md:tracking-normal
-                pb-6 sm:pb-8 md:pb-12
-                px-2 sm:px-4 md:px-0"
-              >
-                Mit niederschwelliger Beratung und praxisnaher Unterstützung
-                schaffen wir berufliche Perspektiven und fördern erfolgreiche
-                Integration.
-              </p>
-            </div>
+          <div className="basis-full mx-auto">
+            <p
+              className="text-3xl xs:text-4xl sm:text-5xl md:text-6xl lg:text-7xl 
+                font-semibold leading-normal tracking-normal
+                pb-20 p-8"
+            >
+              Mit niederschwelliger Beratung und praxisnaher Unterstützung
+              schaffen wir berufliche Perspektiven und fördern erfolgreiche
+              Integration.
+            </p>
           </div>
           <div className="flex flex-col md:flex-row gap-4 md:gap-6 mt-10 md:mt-20 px-4 md:m-4">
             {/* YouTube Video Section */}
