f579d24eda6b9ddbe93b57a1d492e34b19a696b7
Machel G O <machelgabriel@gmail.com>
2025-04-18 12:52:25 +0300

    feat: Update Engagieren page Engagieren Page: - Reposition header section text for better layout - Translate all English text to German - Replace outdated image in Become a Member section - Remove redundant header section from Member page

--- src/components/BecomeMember/BecomeMember.tsx
+++ src/components/BecomeMember/BecomeMember.tsx
@@ -4,30 +4,12 @@ export default function BecomeMember() {
   return (
     <>
       <div>
-        {/* Page Header */}
-        <header
-          className="h-screen w-full bg-cover bg-center relative p-10"
-          style={{ backgroundImage: "url(/bgs/img1.jpg)" }}
-        >
-          <div
-            className={`absolute top-0 left-0 right-0 inset-0 bg-black bg-opacity-60 flex items-start justify-start`}
-          >
-            <h1
-              className={`text-[#fcc41b] drop-shadow-xl text-[8rem] font-extrabold leading-tight uppercase p-6 ml-10 mt-20`}
-            >
-              Become.
-              <br /> Member.
-              <br /> Today.
-            </h1>
-          </div>
-        </header>
-
         {/* Membership form */}
         <section
           id="ehrenamtliches_engagement"
           className="relative h-screen w-full"
         >
-          <div className="absolute inset-0 flex flex-col items-center justify-center mt-20">
+          <div className="absolute inset-0 flex flex-col items-center justify-center">
             <MemberForm />
           </div>
         </section>
