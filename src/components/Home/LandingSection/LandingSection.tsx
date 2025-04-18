d29eb921dc63e6c85052b6dc392969f7a1c4e490
Machel G O <machelgabriel@gmail.com>
2025-04-18 13:23:01 +0300

    feat: Update Home page Home Page: - Reposition header section to bottom-left for layout consistency

--- src/components/Home/LandingSection/LandingSection.tsx
+++ src/components/Home/LandingSection/LandingSection.tsx
@@ -18,27 +18,7 @@ export default function LandingSection() {
       </div>
 
       {/* Text Content */}
-      <div
-        className="absolute bottom-4 left-3 
-            xs:bottom-5 xs:left-4
-            xs2:bottom-6 xs2:left-5
-            sm:bottom-7 sm:left-6
-            sm2:bottom-8 sm2:left-7
-            md:bottom-9 md:left-8
-            md2:bottom-10 md2:left-9
-            tab-sm:bottom-12 tab-sm:left-10
-            tab:bottom-14 tab:left-12
-            tab-lg:bottom-16 tab-lg:left-14
-            tab-xl:bottom-18 tab-xl:left-16
-            lg:bottom-20 lg:left-20
-            lg2:bottom-22 lg2:left-22
-            xl:bottom-24 xl:left-24
-            xl2:bottom-26 xl2:left-28
-            2xl:bottom-28 2xl:left-24
-            3xl:bottom-32 3xl:left-36
-            land-xs:bottom-2 land-xs:left-2
-            land-sm:bottom-4 land-sm:left-4"
-      >
+      <div className="absolute bottom-3 left-4">
         <div>
           <div
             className="text-white space-y-1
