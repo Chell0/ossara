40319e73d04a907abb27fed4f03d4b1fb4a66c1a
Machel G.O.O <machelgabriel@gmail.com>
2025-06-28 15:24:26 +0300

    Chore: Update homepage, the landingSection and images.

--- src/components/Home/LandingSection/LandingSection.tsx
+++ src/components/Home/LandingSection/LandingSection.tsx
@@ -1,23 +1,35 @@
+import Image from "next/image";
+
 export default function LandingSection() {
   return (
-    <>
-      <div className="relative h-screen bg-cover bg-fixed bg-no-repeat bg-center" style={{ backgroundImage: 'url("/bgs/img1.jpg")' }}>
-        <div className="absolute inset-0 bg-black/50">
-          <div className="relative z-10 flex items-end justify-items-start h-full text-white text-left pl-4 pb-8">
-            <h1 className="block text-4xl sm:text-6xl md:text-7xl lg:text-8xl font-bold leading-tight uppercase">
-              <span className="block animate-[slideUp_0.8s_ease-out_0.2s_forwards]">
-                GEMEINSAM GEGEN
-              </span>
-              <span className="block animate-[slideUp_0.8s_ease-out_0.4s_forwards]">
-                Loremipsum dolor
-              </span>
-              <span className="block animate-[slideUp_0.8s_ease-out_0.6s_forwards]">
-                Lorem sit
-              </span>
-            </h1>
-          </div>
+    <section className={`w-full`}>
+      <div className="relative h-screen">
+        <div className="absolute inset-0">
+          <Image
+            src="/bgs/img1.jpg"
+            width={5472}
+            height={3648}
+            alt="Hero Image"
+            className="h-auto w-full object-contain"
+          />
+        </div>
+        <div
+          className="relative z-10 flex items-end justify-end pb-8 pl-4 text-left text-white">
+          <h1
+            className="font-bold text-4xl uppercase leading-tight sm:text-6xl md:text-7xl lg:text-8xl">
+            <span
+              className="block animate-[slideUp_0.8s_ease-out_0.2s_forwards]">
+              GEMEINSAM GEGEN
+            </span>
+            <span
+              className="block animate-[slideUp_0.8s_ease-out_0.4s_forwards]">
+              Loremipsum dolor
+            </span>
+            <span
+              className="block animate-[slideUp_0.8s_ease-out_0.6s_forwards]">Lorem sit</span>
+          </h1>
         </div>
       </div>
-    </>
+    </section>
   );
 }
